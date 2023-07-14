05fcfd055d002353449160af32a287b69efd0e0c
Amine Faouzi <aminefaouzi180@gmail.com>
2023-07-14 17:51:28 +0100

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	while (n = 122 ; n >= 97; ch--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
