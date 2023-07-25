0ecc171e466895db8ccd6ae9a7385d62b40cec70
Amine Faouzi <aminefaouzi180@gmail.com>
2023-07-25 01:49:52 +0100

    something

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	for (i--; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
+	
