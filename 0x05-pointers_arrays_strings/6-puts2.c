5b813c991a68b94d8442e4637eb2cfd5974d484e
Amine Faouzi <aminefaouzi180@gmail.com>
2023-07-25 02:10:11 +0100

    something

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ * @i: integer
+ * Return: Always 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int i;
+	for(i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
