f0394ec91d6e72e4ee1897b692c17db8232c507c
Amine Faouzi <aminefaouzi180@gmail.com>
2023-07-25 02:13:30 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * puts2 - Entry point
- * @i: integer
+ * @i: integeer
+ * @str: striing
  * Return: Always 0 (Success)
  */
 
-void puts2(char *str)
+void puts2 (char *str)
 {
 	int i;
 	for (i = 0; str[i] != '\0'; i++)
