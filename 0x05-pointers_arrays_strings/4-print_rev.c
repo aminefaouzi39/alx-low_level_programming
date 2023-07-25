abe855987cf6a8f06e851acd07018bc87d7fc357
Amine Faouzi <aminefaouzi180@gmail.com>
2023-07-25 01:40:21 +0100

    something

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	for (i--; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
