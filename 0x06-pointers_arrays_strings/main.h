4d573f31cffabfd519635e68349dcfb673366a0a
Amine Faouzi <aminefaouzi180@gmail.com>
2023-07-27 03:52:23 +0100

    something

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 char *_strcat(char *dest, char *src);
-char *_strncpy(char *dest, char *src, int n;)
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 #endif
